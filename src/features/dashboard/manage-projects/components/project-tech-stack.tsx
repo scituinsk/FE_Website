a22e3bbd8d199c75372ab84dfc294fdc61a9672d
Ahmad Zidni Hidayat <zdngaming312@gmail.com>
2025-11-16 22:07:48 +0700

    mend

--- src/features/dashboard/manage-projects/components/project-tech-stack.tsx
+++ src/features/dashboard/manage-projects/components/project-tech-stack.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
 import { X } from "lucide-react";
 import Image from "next/image";
 
