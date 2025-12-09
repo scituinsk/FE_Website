624cddb0e783c7b19226ff9c8ea9488627657ebe
Ahmad Zidni Hidayat <zdngaming312@gmail.com>
2025-12-09 15:44:08 +0700

    feat: add Google Tag Manager integration and update dependencies for improved tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import NextTopLoader from "nextjs-toploader";
 import { Rubik } from "next/font/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
@@ -44,6 +45,7 @@ export default function RootLayout({
       className="scroll-smooth"
       suppressHydrationWarning
     >
+      <GoogleTagManager gtmId="G-GTEMB86C6D" />
       <body className={`${rubik.className} font-sans antialiased`}>
         <NuqsAdapter>
           <ReactQueryProvider>
